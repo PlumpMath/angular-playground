acd189ee676ac7c4c6282c2cb5bf1e9b8811a8c4
Vincent Hellot <hellvinz@gmail.com>
2013-03-22 17:05:07 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('newusersApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('newusersService', ['ngResource']).factory('User', function($resource){
+   return $resource('user', {}, {});
+})
+angular.module('newusersApp',['newusersService']).controller('UserCtrl', function ($scope, User) {
+    $scope.user = User.get();
+});
