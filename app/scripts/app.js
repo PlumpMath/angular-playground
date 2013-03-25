1e60daca3249bb8a37c6e7e92333cc703ef3f2de
Julien Blanchard <julien@sideburns.eu>
2013-03-25 10:11:13 +0100

    WIP

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,28 @@
 'use strict';
 
-angular.module('newusersApp', [])
-  .config(function ($routeProvider) {
+// Fixtures
+var login = {"name":"Léo Tarik"};
+
+// App
+var app = angular.module('newusersApp', [ngMock]);
+
+// Setup fake backend
+app.config(function($provide) {
+  $provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
+});
+
+app.run(function($httpBackend) {
+  $httpBackend.whenJSONP('http://localhost:3000/users/me?callback=JSON_CALLBACK').respond(login);
+});
+
+// Routes
+app.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,21 @@
+var LoginController = function($scope, $http) {
+    $scope.login = {};
+    $scope.login.user = null;
+
+    $scope.login.connect = function() {
+        $http.post('http://localhost:3000/users/me?callback=JSON_CALLBACK').success(function(data, status) {
+            // despite its name, this callback is triggered even in case of an error, so we have to take care
+            if (status < 200 || status >= 300)
+                return;
+            $scope.login.user = data;
+        });
+    };
+
+    $scope.login.disconnect = function() {
+        $scope.login.user = null;
+    };
+
+    $scope.$watch('login.login + login.password', function() {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode($scope.login.login + ':' + $scope.login.password);
+    });
+};
