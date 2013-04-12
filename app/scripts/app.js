ed48564b6790a1812726800f0bce493750a90724
Julien Blanchard <julien@sideburns.eu>
2013-04-12 18:49:27 +0200

    Display some users

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,18 +4,24 @@ var newusersApp = angular.module('newusersApp', []);
 
 newusersApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
-      templateUrl: 'main.html',
+      templateUrl: 'lol.html',
       controller: 'MainCtrl'
-    })
-    .otherwise({
+    }).
+    when('/users', {
+        templateUrl: 'views/list.html',
+        controller: 'UserListCtrl'
+    }).
+    otherwise({
       redirectTo: '/'
     });
 }]);
 
 var newusersAppDev = angular.module('newusersAppDev', ['newusersApp', 'ngMockE2E']);
 newusersAppDev.run(function($httpBackend) {
-  $httpBackend.whenGET(/\.html$/).passThrough();
+    $httpBackend.whenGET(/\.html$/).passThrough();
+    $httpBackend.whenGET(/\.json$/).passThrough();
+    $httpBackend.whenGET('/users').passThrough();
 
-  var user = {id: 1, name: 'Léo Tarik'};
-  $httpBackend.whenPOST('/users/me').respond(user);
+    var user = {id: 1, name: 'Léo Tarik'};
+    $httpBackend.whenPOST('/users/me').respond(user);
 });
--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -1,13 +1,5 @@
-'use strict';
-
-angular.module('newusersService', ['ngResource']).factory('User', function($resource){
-   return $resource('/user', {}, {});
-})
-
-angular.module('newusersApp',['newusersService']).controller('UserCtrl', function ($scope, User) {
-    $scope.user = User.get();
-
-    $scope.user_to_display = function(user) {
-        return user.logged_in ? user.name : "Login"
-    }
-});
+var UserListCtrl = function($scope, $http) {
+    $http.get('users.json').success(function(data) {
+        $scope.users = data;
+    });
+};
